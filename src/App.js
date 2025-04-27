79bb96253d78cf72ef7ff2be9fe4be72236b439d
Kavita Pathak <pathakkavita273@gmail.com>
2025-04-27 22:08:25 +0530

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import './App.css';
 import TextEditor from './Pages/Editor';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <TextEditor />
+      <Routes>
+        <Route path="/editor" element={<TextEditor /> } /> {/* Default route */}
+      </Routes>
     </div>
+  </Router>
   );
 }
 
